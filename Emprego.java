b57dea517e59c04fbc944bb28116bb0311f97348
Gabriel Mello Moraes <47477821+GabrielMello1407@users.noreply.github.com>
2021-01-16 11:47:05 -0300

    Add files via upload

---
+++ Emprego.java
@@ -0,0 +1,26 @@
+
+public class Emprego {
+    
+    private String cargo;
+    private double salario;
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+   
+}
+
